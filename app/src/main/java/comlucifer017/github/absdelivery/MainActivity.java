4411e161170422bd8b89d4d13573d8b9a62ed24f
Irfan Firdaus <irfandauss0@gmail.com>
2018-10-10 15:24:25 +0700

    Add menu done

--- app/src/main/java/comlucifer017/github/absdelivery/MainActivity.java
+++ app/src/main/java/comlucifer017/github/absdelivery/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import butterknife.ButterKnife;
@@ -17,6 +19,31 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        String title = null;
+        switch (item.getItemId()){
+            case R.id.action_kontak:
+                title="Mode List";
+//                showListCountry();
+                break;
+            case R.id.action_home:
+                title="ABSDelivery";
+//                showRecyclerGrid();
+                break;
+//            case R.id.action_cardview:
+//                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @OnClick({R.id.btn_kontak, R.id.btn_menu})
